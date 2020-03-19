c56a91d1535517730f7d060af246b06f5e5dc5cd
KimMooHyeon <dlfb77@gmail.com>
2020-03-19 22:27:02 +0900

    홀수 짝수 판별

---
+++ is_evenly_divisible.py
@@ -0,0 +1,13 @@
+def is_evenly_divisible(number):
+    if number % 2 == 0:
+        return True
+    else:
+
+        return False
+
+    # 코드를 작성하세요
+
+
+print(is_evenly_divisible(3))
+print(is_evenly_divisible(7))
+print(is_evenly_divisible(8))
