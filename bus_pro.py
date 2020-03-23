0e210944204bd9258a227cdb874004f086205b80
KimMooHyeon <dlfb77@gmail.com>
2020-03-24 00:20:57 +0900

    ing

--- bus_pro.py
+++ bus_pro.py
@@ -1,19 +1,15 @@
 T = int(input())
-sol=[]
+
 def sol(K,N,M_list):
-    space = 0
+
     for i in range(0,len(M_list)-1):
         if M_list[i+1]-M_list[i]>K or M_list[0]>K:
             return 0
-    for i in range (0,N):
-        
-
-
-
-
-
+    return  N//K
 
 for test_case in range(1, T + 1):
+
     K,N,M = map(int, input().split())
     M_list = list(map(int, input().split()))
+    print("#{} {}".format(test_case,sol(K,N,M_list)))
 
