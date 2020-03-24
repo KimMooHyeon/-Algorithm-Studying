2f45c1c960ad9cfde843e6faa34ec004debe8b4d
KimMooHyeon <dlfb77@gmail.com>
2020-03-24 23:13:19 +0900

    finish 전기 버스

--- bus_pro.py
+++ bus_pro.py
@@ -1,15 +1,24 @@
 T = int(input())
 
-def sol(K,N,M_list):
+def sol(K,N,M_list) :
+    cnt=0
+    start=0
 
-    for i in range(0,len(M_list)-1):
-        if M_list[i+1]-M_list[i]>K or M_list[0]>K:
-            return 0
-    return  N//K
+    while True:
+        start+=K
+        for i in range(start,start-K,-1):
+            if start>= N:
+                return cnt
+            elif i in M_list:
+                cnt+=1
+                start=i
+                break
+            elif i not in M_list and i == start-K+1:
+                return 0
+
 
 for test_case in range(1, T + 1):
-
-    K,N,M = map(int, input().split())
+    K, N, M = map(int, input().split())
     M_list = list(map(int, input().split()))
-    print("#{} {}".format(test_case,sol(K,N,M_list)))
+    print("#{} {}".format(test_case, sol(K, N, M_list)))
 
