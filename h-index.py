61f2cd6c4192f981dcc255a3d6de3a3a81637a8b
KimMooHyeon <dlfb77@gmail.com>
2020-10-30 00:01:37 +0900

    h-index

---
+++ h-index.py
@@ -0,0 +1,12 @@
+def solution(citations):
+    answer = 0
+    citations.sort()
+    h = 1
+    while(h < len(citations)):
+        temp = len(citations) - h
+        if(temp !=0 and citations[temp] >= h and citations[temp-1] <= h):
+            answer = max(answer,h)
+        h += 1
+    if(answer == 0 and citations[0] >= h):
+        answer = h
+    return answer
