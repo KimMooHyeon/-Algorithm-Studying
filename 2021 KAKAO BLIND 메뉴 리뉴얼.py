fee0578d33e3026fc9eba41912dcee3779f330ae
KimMooHyeon <dlfb77@gmail.com>
2022-06-24 20:55:32 +0900

    Create 2021 KAKAO BLIND 메뉴 리뉴얼.py

---
+++ 2021 KAKAO BLIND 메뉴 리뉴얼.py
@@ -0,0 +1,33 @@
+import itertools
+
+def solution(orders, course):
+    answer=[]
+    menu_dic={}
+    for i in course:
+        for j in range(0,len(orders)):
+            tmp=list(map(list,itertools.combinations(list(orders[j]), i)))
+            for k in tmp :
+                k.sort()
+                menu_dic["".join(k)]=menu_dic.get("".join(k),0)+1
+            #menu_dic[j]=menu_dic.get(j,0)+1
+    real_arr=[]
+
+    for key,value in menu_dic.items():
+        if value>1:
+            real_arr.append([value,key])
+    real_arr.sort(reverse=True)
+    max_num={}
+    print(real_arr)
+    for i in real_arr:
+        if len(i[1]) in max_num :
+            if max_num[len(i[1])] <= i[0]:
+                answer.append(i[1])
+
+        else:
+            max_num[len(i[1])]=i[0]
+            answer.append(i[1])
+            
+            
+    answer.sort()
+      
+    return answer
