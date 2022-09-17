d2bdd32a4c5a1beb31a61759f2f9c8b4ddea7ca9
Mudol2 <dlfb77@gmail.com>
2022-09-17 18:17:28 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 892 ms, Memory: 72052 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.kt
@@ -0,0 +1,23 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.LinkedList
+import java.util.TreeMap
+import java.util.TreeSet
+
+fun main() {
+
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val N = br.readLine().toInt()
+    val nList = mutableListOf<Pair<Int,Int>>()
+    for (i in 0 until N){
+        val (x,y) = br.readLine().split(" ")
+        nList.add(Pair(x.toInt(),y.toInt()))
+    }
+    nList.sortWith(compareBy<Pair<Int, Int>> {it.first}.thenBy { it.second})
+    nList.map { bw.write("${it.first} ${it.second}\n") }
+    br.close()
+    bw.close()
+}
