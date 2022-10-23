a18b1c2bb944c6af858b757848f2012a82ba46dc
Mudol2 <dlfb77@gmail.com>
2022-10-23 13:52:36 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 62.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        var answer: Int = 0
+        return if(num1==num2){
+            1
+        }else{
+            -1
+        }
+        return answer
+    }
+}
