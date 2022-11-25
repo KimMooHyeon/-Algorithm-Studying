541d06fe09c706f11935e75b1e5bf2d8470f459e
Mudol2 <dlfb77@gmail.com>
2022-11-26 03:59:49 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 153.43 ms, Memory: 47.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    val (n) = readLine()!!.split(' ').map(String::toInt)
+    for(i in 1 .. n){
+        for (j in 0 until i){
+            print("*")
+        }
+        println()
+    }
+}
