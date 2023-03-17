a0ef950a43022993006a3f4d4b691507b2a83c2e
Mudol2 <dlfb77@gmail.com>
2023-03-17 16:52:13 +0900

    Time: 43 ms (80.10%), Space: 14.6 MB (10.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,20 +1,9 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        num = []
-        for i in range(0,len(nums)):
-            num.append((nums[i],i))
-
-        num = sorted(num, key=lambda x : x[0])
-        start = 0
-        end = len(num) - 1
-        returnList = []
- 
-        while(start<end):
-            if num[start][0] + num[end][0] == target:
-                returnList = [num[start][1] , num[end][1] ]
-                break
-            elif num[start][0] + num[end][0] < target:
-                start+=1
-            else:
-                end-=1
-        return returnList
+        dict = {}
+        for i,v in enumerate (nums):
+            dict[v]=i
+        for i,v in enumerate (nums) :
+            if target-v in dict and i!=dict[target-v]:
+                return [i,dict[target-v]]
+        return [0]
