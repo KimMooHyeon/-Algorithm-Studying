95ca150fc5dec9cf03b8716dcd388673f08f70fd
Mudol2 <dlfb77@gmail.com>
2023-04-06 18:26:20 +0900

    [Gold V] Title: 호텔, Time: 136 ms, Memory: 20704 KB -BaekjoonHub

---
+++ 백준/Gold/1106. 호텔/호텔.kt
@@ -0,0 +1,47 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+data class CostCity(val cost: Int, val customer: Int)
+
+// 얼리 아답터가 아닌 사람들은 자신의 모든 친구들이 얼리 아답터일 때만 이 아이디어를 받아들인다.
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val (c, n) = br.readLine().split(" ").map { it.toInt() }
+
+
+    val costList = mutableListOf<CostCity>()
+    repeat(n) {
+        // 비용, 비용으로 얻을 수 있는 고객수
+        val city = br.readLine().split(" ").map { it.toInt() }
+        costList.add(CostCity(city[0], city[1]))
+    }
+
+    val dp = IntArray(c+100) { Int.MAX_VALUE }
+    dp[0]=0
+
+    costList.forEach {
+       // println(it)
+        for (i in it.customer until  c+100){
+            if (it.cost == Int.MAX_VALUE || dp[i-it.customer] == Int.MAX_VALUE){
+
+            }else{
+                dp[i] = minOf(dp[i-it.customer]+it.cost,dp[i])
+            }
+
+        }
+    }
+
+    var smallNum = Int.MAX_VALUE
+    for (i in c until  c+100){
+        smallNum= minOf(dp[i],smallNum)
+    }
+    println(smallNum)
+
+
+    br.close()
+    bw.close()
+}
