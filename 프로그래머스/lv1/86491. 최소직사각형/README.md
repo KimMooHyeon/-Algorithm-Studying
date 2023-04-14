4c7f28f33fb63ae506510fa06e555fbd905d2d49
Mudol2 <dlfb77@gmail.com>
2023-04-14 16:20:30 +0900

    [level 1] Title: 최소직사각형, Time: 4.36 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/README.md
+++ 프로그래머스/lv1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
 
 ### 성능 요약
 
-메모리: 70.7 MB, 시간: 12.73 ms
+메모리: 69.8 MB, 시간: 4.36 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.kt
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.kt
@@ -1,18 +1,18 @@
-
+import java.util.*
 class Solution {
     fun solution(sizes: Array<IntArray>): Int {
         var answer: Int = 0
-        var bigList = mutableListOf<Int>()
-        var smallList = mutableListOf<Int>()
+        var first = PriorityQueue<Int>()
+        var second = PriorityQueue<Int>()
         sizes.forEach{
-            if(it[1]>=it[0]){
-                bigList.add(it[1])
-                smallList.add(it[0])
+            if(it[0]>it[1]){
+                first.add(-it[0])
+                second.add(-it[1])
             }else{
-                bigList.add(it[0])
-                smallList.add(it[1])
+                first.add(-it[1])
+                second.add(-it[0])
             }
         }
-        return bigList.maxOrNull()!!*smallList.maxOrNull()!!
+        return first.poll() * second.poll()
     }
 }
